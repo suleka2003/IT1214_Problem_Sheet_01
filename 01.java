7b12c818a975b1e6a5f55a391797a562a2d31386
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-05-17 10:27:48 +0000

    new

--- 01.java
+++ 01.java
@@ -1,3 +1,40 @@
-public class 02 {
+class 01 {
+
+    private int studentID;
+    private String name;
+    private int daysAttended;
     
+    public Student (int studentID, String name, int daysAttended) {
+        setStudentID(studentID);
+        setName(name);
+        setDaysAttended(daysAttended);
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDaysAttended() {
+        return daysAttended;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDaysAttended(int daysAttended) {
+        this.daysAttended = daysAttended;
+    }
+
+    public static void main (String[] args) {
+
+    }
 }
